bf36ac13954630fa53843b7d5b145d0b91a40671
Chaplin16 <chaplin16@orange.fr>
2021-10-06 23:06:35 +0200

    ajout de naws avis

--- js/comments.js
+++ js/comments.js
@@ -1,26 +1,50 @@
 const comments = [
     {
-        id: 1,
+        id: 12,
+        comment: "Nous avons pris Joseph pour notre mariage, le baptême de nos enfants et des fêtes familiales. Il est à l'écoute et s'adapte à nos envies, nos besoins et notre budget. C'est un plaisir de faire appel à ses services et c'est toujours très bon et très copieux. Je recommande.",
+        author:"Angélique",
+        town:"Mauguio" 
+    },
+    {
+        id: 12,
+        comment: "Traiteur complice de vos réceptions, buffets, apéros... large gamme de produits adaptée à vos envies. Rendez-vous des gourmets et des gourmands! ",
+        author:"Sandrine",
+        town:"Strasbourg" 
+    },
+    {
+        id: 11,
+        comment: "Excellent rapport qualité/prix, une ambiance familiale, un grand sérieux et du professionnalisme. Je recommande pour toutes sortes de cérémonies. Bonne organisation: BRAVO!",
+        author:"Isabelle",
+        town:"Mauguio" 
+    },
+    {
+        id: 10,
+        comment: "Que de belles prestations! Très bonne cuisine, Joseph excelle dans son domaine.",
+        author:"André",
+        town:"Mauguio" 
+    },
+    {
+        id: 9,
         comment: "Buffet d'une très grande qualité autant au niveau visuel que gustatif, une mise en place et un service irréprochable!",
         author:"Emma",
         town:"Montpellier"
     },
     {
-        id: 2,
+        id: 8,
         comment: "C'était vraiment bien! Tout était parfait! Merci Joseph pour ce délicieux repas pour le baptême de mes enfants",
         author:"Aurore",
         town:"Aigues-Mortes" 
     },
     {
-        id: 3,
+        id: 7,
         comment: "Excellent! Je recommande!",
         author:"Jérémy",
         town:"Lansargues" 
     },
     {
-        id: 4,
+        id: 6,
         comment: "Très bon rapport qualité/prix, nous nous sommes régalés. C'était très copieux, il en restait! ",
-        author:"Estelle",
+        author:"Marie",
         town:"Sommières" 
     },
     {
@@ -30,25 +54,25 @@ const comments = [
         town:"Nîmes" 
     },
     {
-        id: 6,
+        id: 4,
         comment: "Prix complètement abordables, la nourriture est excellente",
         author:"Lydia",
         town:"Lunel" 
     },
     {
-        id: 7,
+        id: 3,
         comment: "Top service, très bonne nourriture pour un bon prix",
         author:"Sylvie",
         town:"Nîmes" 
     },
     {
-        id: 8,
+        id: 2,
         comment: "Joseph est un super traiteur, franchement! il connaît très bien son métier, faites lui confiance, vous vous régalerez",
-        author:"Jean-Paul",
+        author:"Jean-Pierre",
         town:"Castries" 
     },
     {
-        id: 9,
+        id: 1,
         comment: "Un délice pour les yeux et les papilles...",
         author:"Valérie",
         town:"Aigues-Mortes" 
