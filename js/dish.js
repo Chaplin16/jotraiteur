d63393cf3942ae2eb8771889dd776b6efdd15424
Chaplin16 <chaplin16@orange.fr>
2021-10-07 12:19:09 +0200

    recette cards

--- js/dish.js
+++ js/dish.js
@@ -2,41 +2,51 @@ const dish  = [
     {
         id:1,
         name: "Brasucade",
-        description:"Moules parfumées cuites sur le feu et arrosées d'une marinade"
+        description:"Moules parfumées cuites sur le feu et arrosées d'une marinade de légumes"
     },
     {
         id:2,
         name: "Fideua",
-        description:"Plat catalan de pâtes et fruits de mer (gambas, moules et seiches)"
+        description:"Plat catalan de pâtes Fidéo grillée à l'ail et fruits de mer (crevettes, calamars, lotte et autres poissons)"
     },
     {
         id:3,
-        name: "Tajine",
-        description:"Poulet ou agneau"
+        name: "Tajine aux pruneaux et amandes",
+        description:"Légumes, semoule et viande (agneau ou poulet)"
     },
     {
         id:4,
-        name: "Macaronnade Catalane, Sétoise...",
-        description:"Plat avec des brageoles et des macaronis"
+        name: "Macaronnade Catalane",
+        description:"Penne rigate avec poulet, porc et saucisses"
+    },
+    {
+        id:4,
+        name: "Macaronnade Poisson",
+        description:"Penne rigate avec queue de lotte et colin"
+    },
+    {
+        id:4,
+        name: "Macaronnade Sétoise",
+        description:"Penne rigate avec paleron et saucisses (viande cuite à part)"
     },
     {
         id:5,
         name: "Rouille de seiche",
-        description:"Spécialitée sétoise, servie avec de l'aioli"
+        description:"Spécialité sétoise, servie avec de l'aioli"
     },
     {
         id:6,
-        name: "Encornets farcis",
-        description:"Farcis à la viande, à la sétoise ou à la provençale"
+        name: "Encornets farcis à la Provençale",
+        description:"Encornets farcies de 2/3 de farce porc et 1/3 de boeuf avec un écrasée de pomme de terre ou riz"
     },
     {
         id:7,
         name: "Gardianne de Taureau",
-        description:"Plat traditionnel camarguais typique"
+        description:"Plat traditionnel Camarguais. Viande de taureaux cuisinée au vin rouge (façon daube) "
     },
     {
         id:8,
-        name: "Paella Espagnole",
-        description:"Au poulet, au chorizo et aux fruits de mer"
+        name: "Paella Catalane",
+        description:"Paella poulet, porc, seiche et fruits de mer"
     },
 ]
